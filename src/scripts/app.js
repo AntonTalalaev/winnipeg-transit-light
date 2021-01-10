3ba4df6af49ba233c3daf7e3f92e659fb556d9bb
Anton Talalaev <anton.talalaev@evotrux.com>
2021-01-10 14:26:25 -0600

    Adds comments and error messages

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -60,8 +60,6 @@ const minTimeTripButton = document.querySelector('.min-time-trip');
 const minTransferTripButton = document.querySelector('.min-transfer-trip');
 const minWalkTripButton = document.querySelector('.min-walk-trip');
 
-
-
 /**
  * Method to make API calls and return JSON
  * @param {string} url - URL string for API call
@@ -72,19 +70,15 @@ function fetchJSON(url) {
       if (resp.ok) {
         return resp.json();
       } else {
-        Promise.reject({ resp: resp.status, resp: resp.statusText });
+        throw new Error(`Status - #${resp.status}; status message - ${resp.statusText}.`);
       }
     })
-    .catch(err => {
-      // message to the user
-      console.log(err);
-    });
 };
 
 /**
- * 
- * @param {*} places 
- * @param {*} listElemet 
+ * Method to display on the page found places
+ * @param {JSON} places - places that were found based on search
+ * @param {DOM element} listElemet - container were to show found places
  */
 const displayPlacesElements = function (places, listElemet) {
   places.forEach(place => {
@@ -103,47 +97,83 @@ const displayPlacesElements = function (places, listElemet) {
   });
 }
 
-
-const displayNoPlacesFoundMessage = function () {
-  // display error to the user, ask to reenter query for search
+/**
+ * Method to display 'Places not found' error message
+ */
+const displayNoPlacesFoundMessage = function (listElemet) {
   console.log('Error: places not found');
-}
+  listElemet.insertAdjacentHTML('beforeend',
+    `<li style="background-color:#f2f3ff;">
+        <div style="margin-bottom:5px;">We are very sorry, but based on your search, 
+        no places were found in Winnipeg.</div>
+        <div>Please try to change the query string.</div>
+    </li>`
+  );
+};
 
+/**
+ * Method to display 'Server error' error message
+ */
+const displayCustomErrorMessage = function (message) {
+  busContainerElement.innerHTML =
+    `<div style="border:#c91717 1px solid; color:#b03647; padding:10px;">
+        <p>We are sorry, but an error was emitted.</p>
+        <p>Type of error: ${message}.</p>
+        <p>Please, try again.</p>
+    </div>`;
+};
+
+/**
+ * Method to call API to get places and display found places on the page
+ * @param {string} url - url for API call (Mapbox)
+ * @param {DOM element} listElemet - container were to show found places
+ */
 const showPlaces = function (url, listElemet) {
   fetchJSON(url)
     .then(json => {
-      if (json.features.length === 0) {
-        displayNoPlacesFoundMessage();
+      if (json === undefined
+        || json.features === undefined) {
+        throw new Error('Server error');
+      } else if (json.features.length === 0) {
+        displayNoPlacesFoundMessage(listElemet);
       } else {
         displayPlacesElements(json.features, listElemet);
       }
     })
     .catch(err => {
-      // display message to user
+      displayCustomErrorMessage('Server error');
       console.log(err);
     });
-}
+};
 
+/**
+ * Method to validate user input
+ * @param {string} input - users input for search places
+ */
 const validateInput = function (input) {
   format = /[!@#$%^&*()_+\=\[\]{};\\|<>\/?]+/;
   if (input === undefined || input === null || input === "") {
     return false;
   } else if (format.test(input)) {
-    // show error for user
+    displayCustomErrorMessage('Invalid input - special characters were used');
     console.log('Error: special characters used in input');
     return false;
   }
   return true;
-}
-
-const clearElementHTML = function (elemet) {
-  elemet.innerHTML = '';
-}
+};
 
 /**
- * 
- * @param {*} inputElement 
- * @param {*} listElemet 
+ * Method to clean all HTML inside the element
+ * @param {DOM element} elemet - DOM element
+ */
+const clearElementHTML = function (elemet) {
+  elemet.innerHTML = '';
+};
+
+/**
+ * Method to search places based on users input
+ * @param {DOM element} inputElement - input DOM element 
+ * @param {DOM element} listElemet - container were to show found places
  */
 const searchPoints = function (inputElement, listElemet) {
   clearElementHTML(listElemet);
@@ -151,11 +181,10 @@ const searchPoints = function (inputElement, listElemet) {
     const url = mapboxURLBase + inputElement.value + mapboxURLArgs;
     showPlaces(url, listElemet);
   }
-}
-
+};
 
 /**
- * EventListener for search origin point
+ * EventListener to search origin point
  */
 originInput.addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
@@ -164,7 +193,7 @@ originInput.addEventListener('keyup', function (event) {
 });
 
 /**
- * EventListener for search origin point
+ * EventListener to search destination point
  */
 destinationInput.addEventListener('keyup', function (event) {
   if (event.keyCode === 13) {
@@ -173,9 +202,9 @@ destinationInput.addEventListener('keyup', function (event) {
 });
 
 /**
- * 
- * @param {*} element 
- * @param {*} listElemet 
+ * Method to highlight selected place element
+ * @param {DOM element} element - place element
+ * @param {DOM element} listElemet - container with found places
  */
 const selectPlace = function (element, listElemet) {
   if (element.tagName.toLowerCase() === 'li'
@@ -197,25 +226,34 @@ const selectPlace = function (element, listElemet) {
 };
 
 /**
- * EventListener for selecting origin point to route
+ * EventListener to select origin point (place)
  */
 originsListElemet.addEventListener('click', function (event) {
   selectPlace(event.target, originsListElemet);
 });
 
 /**
- * EventListener for selecting destination point to route
+ * EventListener to select destination point (place)
  */
 destinationsListElemet.addEventListener('click', function (event) {
   selectPlace(event.target, destinationsListElemet);
 });
 
-
+/**
+ * Method to display 'No Routes Found' error message
+ */
 const displayNoRoutesFoundMessage = function () {
-  //
+  busContainerElement.innerHTML =
+    `<div style="padding:10px;">
+        <p>No available routes found.</p>
+        <p>Please, try again.</p>
+    </div>`;
 };
 
-
+/**
+ * Method to generate Walk Segment of the trip
+ * @param {JSON} segment - JSON object that represents part(segment) of the trip
+ */
 const getWalkSegment = function (segment) {
   const duration = segment.times.durations.total = 2;
   let destStr = '';
@@ -224,38 +262,52 @@ const getWalkSegment = function (segment) {
   } else if (segment.to.destination !== undefined) {
     destStr = `your destination`;
   }
+
   return `<li>
             <i class="fas fa-walking" aria-hidden="true"></i>Walk for ${duration} 
             minutes to ${destStr}.
           </li>`;
 };
 
+/**
+ * Method to generate Ride Segment of the trip
+ * @param {JSON} segment - JSON object that represents part(segment) of the trip
+ */
 const getRideSegment = function (segment) {
   const duration = segment.times.durations.total;
   const routeName = segment.route.name;
+
   return `<li>
             <i class="fas fa-bus" aria-hidden="true"></i>Ride the ${routeName} 
             for ${duration} minutes.
           </li>`;
 };
 
-
+/**
+ * Method to generate Transfer Segment of the trip
+ * @param {JSON} segment - JSON object that represents part(segment) of the trip
+ */
 const getTransferSegment = function (segment) {
   const fromStop = segment.from.stop;
   const toStop = segment.to.stop;
+
   return `<li>
             <i class="fas fa-ticket-alt" aria-hidden="true"></i>Transfer from stop
             #${fromStop.key} - ${fromStop.name} to stop #${toStop.key} - ${toStop.name}.
           </li>`;
 };
 
-
+/**
+ * Method to generate the trip element HTML
+ * @param {JSON} route - JSON object that represents the trip(route)
+ */
 const createRouteHTML = function (route) {
   let routeHTML = `<ul class="my-trip hide" 
             data-durationTotal="${route.times.durations.total}" 
             data-durationWalking="${route.times.durations.walking}"  
             data-numOfSegments="${route.segments.length}"
         >`;
+
   route.segments.forEach(segment => {
     if (segment.type === 'walk') {
       routeHTML += getWalkSegment(segment);
@@ -265,29 +317,39 @@ const createRouteHTML = function (route) {
       routeHTML += getTransferSegment(segment);
     }
   });
+
   routeHTML += '</ul>';
+
   return routeHTML;
 };
 
+/**
+ * Method to insert route(trip) elements HTML into the container
+ * @param {[JSON]} routes - array of JSON objects that represents found trips(routes)
+ */
 const insertRouteElements = function (routes) {
   routes.forEach(route => {
     busContainerElement.insertAdjacentHTML('beforeend', createRouteHTML(route));
   });
 };
 
-
+/**
+ * Method to choose and mark the recommended route(trip)
+ */
 const setRecommendedRoute = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
   let minDuration = Number.MAX_SAFE_INTEGER;
   let bestTrip;
+
   myTripElements.forEach(trip => {
     if (minDuration > trip.dataset.durationtotal) {
       minDuration = trip.dataset.durationtotal;
     }
   });
-  minDuration += minDuration * 10 / 100;
+
   const duration = minDuration + minDuration * timePrecision;
   let minTransfer = Number.MAX_SAFE_INTEGER;
+
   myTripElements.forEach(trip => {
     if (trip.dataset.durationtotal <= duration) {
       if (trip.dataset.durationtotal === minDuration) {
@@ -298,6 +360,7 @@ const setRecommendedRoute = function () {
       }
     }
   });
+
   myTripElements.forEach(trip => {
     if (trip.dataset.durationtotal <= duration) {
       if (trip.dataset.numofsegments === minTransfer) {
@@ -305,66 +368,95 @@ const setRecommendedRoute = function () {
       }
     }
   });
+
   bestTrip.classList.add('recommended');
   bestTrip.classList.remove('hide');
-}
+};
 
-
+/**
+ * Method to call API to get routes(trips) and display results on the page
+ * @param {string} url - url for API call (Winnipeg Transit)
+ */
 const showRoute = function (url) {
   fetchJSON(url)
     .then(json => {
-      if (json.plans.length === 0) {
+      if (json === undefined
+        || json.plans === undefined
+        || json.plans.length === 0) {
         displayNoRoutesFoundMessage();
       } else {
+        console.log(json);
         insertRouteElements(json.plans);
         setRecommendedRoute();
+        showFilterButtons();
       }
     })
     .catch(err => {
-      // display message to user
+      displayCustomErrorMessage('Server error');
       console.log(err);
     });
+};
 
-}
-
-
+/**
+ * Method to generate location string of the place for API call (Winnipeg Transit)
+ * @param {DOM element} place - DOM element that represents place(point)
+ */
 const getLocationStr = function (place) {
   const placeLat = place.dataset.lat;
   const placeLon = place.dataset.long;
+
   return `geo/${placeLat},${placeLon}`;
-}
+};
 
-
+/**
+ * Method to generate string for TripPlanner API call (Winnipeg Transit)
+ */
 const getTripPlannerURL = function () {
   const originLocationStr = getLocationStr(originsListElemet.querySelector('.selected'));
-  const destinationLocationStr = getLocationStr(destinationsListElemet.querySelector('.selected'));
-  const url = `${tripPlannerURL}&origin=${originLocationStr}&destination=${destinationLocationStr}`;
+  const destinationLocationStr =
+    getLocationStr(destinationsListElemet.querySelector('.selected'));
+  const url =
+    `${tripPlannerURL}&origin=${originLocationStr}&destination=${destinationLocationStr}`;
+
   return url;
 };
 
-
-planTripElement.addEventListener('click', function (event) {
+/**
+ * Event Listener for Plan My Trip button
+ */
+planTripElement.addEventListener('click', function () {
   // If there isn't at least 1 starting location and 1 destination selected, clicking the plan my trip button should display an appropriate message to the user. 
+  // If locations are the same - message that you choose the same start and finish points
   const url = getTripPlannerURL();
   clearElementHTML(busContainerElement);
+  hideFilterButtons();
   showRoute(url);
-  showFilterButtons();
 });
-
-
-
 
 /**
  ***************** Filters
  */
 
-
+/**
+ * Method to show Filter Buttons
+ */
 function showFilterButtons() {
   filtersContainerElement.classList.remove('hide');
 };
 
+/**
+ * Method to hide Filter Buttons
+ */
+function hideFilterButtons() {
+  filtersContainerElement.classList.add('hide');
+};
+
+/**
+ * Method to show only Recommended Trip
+ */
 const showRecommendedTripOnly = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
+
   myTripElements.forEach(trip => {
     if (trip.classList.contains('recommended')) {
       trip.classList.remove('recommended-border');
@@ -375,13 +467,19 @@ const showRecommendedTripOnly = function () {
   });
 };
 
-
+/**
+ * Event Listener for Best Trip button
+ */
 bestTripButton.addEventListener('click', function () {
   showRecommendedTripOnly();
 });
 
+/**
+ * Method to show all Trips
+ */
 const showAllTrips = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
+
   myTripElements.forEach(trip => {
     if (trip.classList.contains('recommended')) {
       trip.classList.add('recommended-border');
@@ -390,15 +488,20 @@ const showAllTrips = function () {
   });
 };
 
+/**
+ * Event Listener for All Trips button
+ */
 allTripsButton.addEventListener('click', function () {
   showAllTrips();
 });
 
-
-
+/**
+ * Method to show Min Time Trips
+ */
 const showMinTimeTrips = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
   let minTime = Number.MAX_SAFE_INTEGER;
+
   myTripElements.forEach(trip => {
     trip.classList.remove('recommended-border');
     trip.classList.add('hide');
@@ -406,20 +509,28 @@ const showMinTimeTrips = function () {
       minTime = trip.dataset.durationtotal;
     }
   });
+
   myTripElements.forEach(trip => {
     if (trip.dataset.durationtotal === minTime) {
       trip.classList.remove('hide');
     }
   });
-}
+};
 
+/**
+ * Event Listener for Min Time Trip button
+ */
 minTimeTripButton.addEventListener('click', function () {
   showMinTimeTrips();
 });
 
+/**
+ * Method to show Min Transfer Trips
+ */
 const showMinTransferTrips = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
   let minTransfer = Number.MAX_SAFE_INTEGER;
+
   myTripElements.forEach(trip => {
     trip.classList.remove('recommended-border');
     trip.classList.add('hide');
@@ -427,35 +538,45 @@ const showMinTransferTrips = function () {
       minTransfer = trip.dataset.numofsegments;
     }
   });
+
   myTripElements.forEach(trip => {
     if (trip.dataset.numofsegments === minTransfer) {
       trip.classList.remove('hide');
     }
   });
-}
+};
 
-
-
+/**
+ * Event Listener for Min Transfer Trip button
+ */
 minTransferTripButton.addEventListener('click', function () {
   showMinTransferTrips();
 });
 
+/**
+ * Method to show Min Walk Trips
+ */
 const showMinWalkTrips = function () {
   const myTripElements = busContainerElement.querySelectorAll('.my-trip');
   let minWalk = Number.MAX_SAFE_INTEGER;
+
   myTripElements.forEach(trip => {
     trip.classList.add('hide');
     if (minWalk > trip.dataset.durationwalking) {
       minWalk = trip.dataset.durationwalking;
     }
   });
+  
   myTripElements.forEach(trip => {
     if (trip.dataset.durationwalking === minWalk) {
       trip.classList.remove('hide');
     }
   });
-}
+};
 
+/**
+ * Event Listener for Min Walk Trip button
+ */
 minWalkTripButton.addEventListener('click', function () {
   showMinWalkTrips();
 });
